9ffdebd875921917a2aab4f565997a1aff8d49d4
creativetemmy <temitopeayo01@gmail.com>
2023-08-24 00:31:28 +0100

    Added styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,44 @@
-import { headerLogo } from '../assets/images'
-import { hamburger } from '../assets/icons'
-import { navLinks } from '../constants'
+import React, { useState } from 'react'
 
-function Nav() {
+import { navLinks } from '../constants'
+import { headerLogo } from '../assets/images'
+
+const Nav = () => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
-      <nav className="flex justify-between items-center max-container ">
-        <a href="/">
-          <img src={headerLogo} alt="Logo" width={130} height={29} />
-        </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+    <header className="shadow-md w-full fixed top-0 left-0 z-10">
+      <nav className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7 max-container border ">
+        <div
+          className="font-bold text-2xl cursor-pointer flex items-center font-montserrat
+      text-gray-800"
+        >
+          <img src={headerLogo} alt="logo" />
+        </div>
+
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-2 cursor-pointer md:hidden "
+        >
+          <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
+        </div>
+
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto  pl-9 transition-all duration-500 ease-in ${
+            open ? 'top-15 ' : 'top-[-490px]'
+          }`}
+        >
           {navLinks.map((item) => (
-            <li key={item.label}>
+            <li key={item.label} className="md:ml-8 text-xl md:my-0 my-7">
               <a
                 href={item.href}
-                className="font-montserrat leading-normal text-lg text-slate-gray"
+                className="text-gray-800 hover:text-coral-red duration-500"
               >
                 {item.label}
               </a>
             </li>
           ))}
         </ul>
-        <div className="hidden max-lg:block">
-          <img src={hamburger} alt="hamburger" width={25} height={25} />
-        </div>
       </nav>
     </header>
   )
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -20,9 +20,10 @@ const Hero = () => {
         </p>
 
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
-            The New Arrival
+          <span className="xl:bg-white xl:whitespace-nowrap relative  pr-10">
+            New
           </span>
+          Blazing
           <br />
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
@@ -51,7 +52,7 @@ const Hero = () => {
           alt="shoe collection"
           width={610}
           height={502}
-          className="object-contain relative z-10"
+          className="object-contain relative "
         />
 
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
